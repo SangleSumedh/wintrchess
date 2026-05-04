da5efaebba7ff626555ed69a10c9742f7355b9f9
Sumedh Sangle <sanglesumedh15@gmail.com>
2026-05-04 22:43:45 +0530

    Polish analysis UI with SumChess branding

--- client/src/apps/features/analysis/components/AnalysisPanel/AnalysisProgress/index.tsx
+++ client/src/apps/features/analysis/components/AnalysisPanel/AnalysisProgress/index.tsx
@@ -43,7 +43,7 @@ function AnalysisProgress() {
         }
 
         function focusListener() {
-            document.title = "WintrChess";
+            document.title = "SumChess";
             removeEventListener("focus", focusListener);
         }
 
